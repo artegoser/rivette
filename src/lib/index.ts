c55339736c6289c9fefe39f9541da1614267a92a
Artemy Egorov <artegoser@gmail.com>
2025-06-04 15:09:25 +0300

    add exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,9 @@
 import * as rive from '@rive-app/canvas';
 import type { RiveParameters } from '@rive-app/canvas';
 
+export { default as Rivette } from './Rivette.svelte';
+export { default as RawCanvas } from './RawCanvas.svelte';
+
 export type EventCallback = (event: rive.Event, rive_instance: rive.Rive) => void;
 
 export type RawCanvasProps = Omit<RiveParameters, 'canvas' | 'onload' | 'onLoad'> &
